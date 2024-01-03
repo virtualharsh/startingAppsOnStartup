5cd622638a7b42e21c62608713fdc99e3533fa63
harsh <imharshsonegra9577@gmail.com>
2024-01-03 23:17:53 +0530

    Initial commit

---
+++ startup.py
@@ -0,0 +1,6 @@
+from os import system
+
+
+system("start msedge file:///C:/Users/imhar/OneDrive/Desktop/life_is_short.pdf")
+system("start chrome https://youtube.com/playlist?list=PLu0W_9lII9agq5TrH9XLIKQvv0iaF2X3w ")
+system(f"code C:/Users/imhar/OneDrive/Desktop/front-end")
